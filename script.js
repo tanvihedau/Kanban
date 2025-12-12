0dcdfd31f8cdfc4b04627f0a63c6d06dc8da3ced
tanvihedau <tanvihedau28@gmail.com>
2025-12-12 23:26:59 +0530

    update

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
-const todo = document.querySelector('#todo');
-const progress = document.querySelector('#progress');
-const done = document.querySelector('#done');
+const todo = document.querySelector("#todo");
+const progress = document.querySelector("#progress");
+const done = document.querySelector("#done");
+
+let draggedElement = null;
 
 console.log(todo, progress, done);
 
-const tasks = document.querySelectorAll('.task');
+const tasks = document.querySelectorAll(".task");
 
-tasks.forEach(task => {
-    task.addEventListener("drag", (e)=>{
-     
-    });
-   
+tasks.forEach((task) => {
+  task.addEventListener("drag", (e) => {
+    console.log("dragging", e);
+    draggedElement=task;
+  });
 });
 
-function addDragEventsOnColumn(column){
-    column.addEventListener("dragenter", (e)=>{
-        e.preventDefault();
-        column.classList.add("hover-over");
-    })
-    column.addEventListener("dragleave", (e)=>{
-        e.preventDefault();
-        column.classList.remove("hover-over");
-    })
+function addDragEventsOnColumn(column) {
+  column.addEventListener("dragenter", (e) => {
+    e.preventDefault();
+    column.classList.add("hover-over");
+  });
+  column.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    column.classList.remove("hover-over");
+  });
 
-    column.addEventListener("dragover", (e)=>{
-        e.preventDefault();
-    })
+  column.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
 
-    column.addEventListener("drop", (e)=>{
-        e.preventDefault();
-        console.log("Dropped" , e);
-    })
+  column.addEventListener("drop", (e) => {
+    e.preventDefault();
+    console.log("Dropped", draggedElement, column);
+  });
 }
 
 addDragEventsOnColumn(todo);
