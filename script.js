dd7e8058977bf329b169725e958711e4425e5228
tanvihedau <tanvihedau28@gmail.com>
2025-12-17 00:10:29 +0530

    updated

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const tasks = document.querySelectorAll(".task");
 
 tasks.forEach((task) => {
   task.addEventListener("drag", (e) => {
-    console.log("dragging", e);
-    draggedElement=task;
+    // console.log("dragging", e);
+    draggedElement = task;
   });
 });
 
@@ -32,9 +32,27 @@ function addDragEventsOnColumn(column) {
   column.addEventListener("drop", (e) => {
     e.preventDefault();
     console.log("Dropped", draggedElement, column);
+    column.appendChild(draggedElement);
+    column.classList.remove("hover-over");
   });
 }
 
 addDragEventsOnColumn(todo);
 addDragEventsOnColumn(progress);
 addDragEventsOnColumn(done);
+
+/*modal related logic*/ 
+
+const toggleModalButton= document.querySelector("#toggle-modal");
+const modalBg = document.querySelector(".modal .bg");
+const modal= document.querySelector(".modal");
+
+toggleModalButton.addEventListener("click", ()=>{
+ modal.classList.toggle("active");
+})
+// modalBg.addEventListener("click", ()=>{
+//  modal.classList.remove("active");
+// })
+
+
+ /*modal related logic*/   
